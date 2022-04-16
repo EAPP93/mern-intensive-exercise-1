a77bf012cdfca85efb032d7d2b857bc82bfa50b7
EAPP93 <empireeapp@gmail.com>
2022-04-16 07:16:17 +0200

    documentacion readme.md terminada

---
+++ __test__/example.spec.ts
@@ -0,0 +1,5 @@
+describe('mis primeras pruebas',()=>{
+    it('mis primeras pruebas', ()=>{
+        expect(true).toBe(true);
+    })
+})
---
+++ index.ts
@@ -0,0 +1,26 @@
+import express,{ Express , Request , Response } from "express";
+import dotenv from 'dotenv'
+
+//configuramos archivos .env
+dotenv.config();
+
+//creamos una aplicacion express
+const app:Express = express();
+const port:String | Number = process.env.PORT || 8000; 
+
+//Define the first Route of Appp
+app.get('/',(req: Request,res:Response) =>{
+    //Send Hello World
+    res.send('Welcome to API Restful: Express + TS + Nodemon + Jest + Swagger + Mongoose')
+})
+
+
+app.get('/hello',(req: Request,res:Response) =>{
+    //Send Hello World
+    res.send('Hello world')
+})
+
+// Execute APP and listen Requests to PORT
+app.listen(port, ()=>{
+    console.log(`Express Server: Running at http://localhost:${port}`)
+})
